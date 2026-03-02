50b6455a792907ea88ef3d5ddff888cecbdcaca8
Bill Major <robe5155@esri.com>
2026-03-02 09:24:59 -0500

    Updates

--- organizationsettings.py
+++ organizationsettings.py
@@ -10,19 +10,19 @@ def update_security_settings(portal_url, username, password, security_settings):
         exit(1)
 
     print(f"Updating Organization Settings if different.")
-    security_settings = gis.admin.ux.security_settings
+    current_security_settings = gis.admin.ux.security_settings
     system_properties = gis.admin.system.properties
     disabled_creation_builtin_accounts = system_properties['disableSignup']
  
     # Apply Changes if different
-    if security_settings.anonymous_access != git_settings['anonymous_access']:
-        security_settings.anonymous_access = git_settings['anonymous_access']
-    if str(security_settings.enable_https) != git_settings['enable_https']:
-        security_settings.enable_https = ast.literal_eval(git_settings['enable_https'])
-    if str(security_settings.show_social_media) != git_settings['show_social_media']:
-        security_settings.show_social_media = ast.literal_eval(git_settings['show_social_media'])
-    if str(disabled_creation_builtin_accounts) != git_settings['disableSignup']:
-        system_properties['disableSignup'] = git_settings['disableSignup']
+    if current_security_settings.anonymous_access != security_settings['anonymous_access']:
+        current_security_settings.anonymous_access = security_settings['anonymous_access']
+    if str(current_security_settings.enable_https) != security_settings['enable_https']:
+        current_security_settings.enable_https = ast.literal_eval(security_settings['enable_https'])
+    if str(current_security_settings.show_social_media) != security_settings['show_social_media']:
+        current_security_settings.show_social_media = ast.literal_eval(security_settings['show_social_media'])
+    if str(disabled_creation_builtin_accounts) != security_settings['disableSignup']:
+        system_properties['disableSignup'] = security_settings['disableSignup']
         gis.admin.system.properties = system_properties
         
     print("Organization Settings Updated successfully.")
